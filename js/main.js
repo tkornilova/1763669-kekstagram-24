7c872ecdddbd973975822096b5c638984edde36e
tkornilova <kornilova.tat07@gmail.com>
2021-09-25 20:07:27 -0600

    Выбор случайного числа

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Выбор случайного числа от min до (max+1)
+// Source: https://learn.javascript.ru/task/random-int-min-max
+
+function randomIntegerNumber (min, max) {
+  if (min < 0 || max < 0) {
+    console.log('Минимальное и максимальные значения должны быть больше 0');
+    return;
+  }
+
+  if (min > max) {
+    console.log('Минимальное значение должно быть меньше максимального');
+    return;
+  }
+
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+
+randomIntegerNumber(4, 15);
