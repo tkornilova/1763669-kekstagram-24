8c74a1b0e382b3986aa870b88a33ef1302dcbee9
tkornilova <kornilova.tat07@gmail.com>
2021-09-27 11:16:30 -0600

    Исправления ошибок

--- js/main.js
+++ js/main.js
@@ -3,17 +3,16 @@
 
 function randomIntegerNumber (min, max) {
   if (min < 0 || max < 0) {
-    console.log('Минимальное и максимальные значения должны быть больше 0');
-    return;
+    return('Минимальное и максимальные значения должны быть больше 0');
+
   }
 
   if (min > max) {
-    console.log('Минимальное значение должно быть меньше максимального');
-    return;
+    return('Минимальное значение должно быть меньше максимального');
   }
 
   const rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
+  return(Math.floor(rand));
 }
 
 randomIntegerNumber(4, 15);
@@ -21,14 +20,15 @@ randomIntegerNumber(4, 15);
 
 // Проверка длины комментария
 
+
 const maxLength = 140;
 let userCommentLength;
 let strokeNumber;
 
 function commentLength (strokeNumber, maxLength) {
   if (maxLength > userCommentLength) {
-    console.log('Длина комментария превышает максимально допустимую длину');
+    return('Длина комментария превышает максимально допустимую длину');
   }
 }
 
-commentLength (5, 40);
+commentLength (strokeNumber, maxLength);
