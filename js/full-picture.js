3a5df617d467c97b532caf2c037d7dd728aaf8dd
tkornilova <kornilova.tat07@gmail.com>
2021-10-25 21:42:38 -0600

    Исправления

--- js/full-picture.js
+++ js/full-picture.js
@@ -2,12 +2,24 @@ import {photoDescriptions} from './data.js';
 import {comments} from './data.js';
 
 const fullPicture = document.querySelector('.big-picture');
-const userMiniature = document.querySelector('.picture');
 
-// Открытие окна
-userMiniature.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  fullPicture.classList.remove('hidden');
+// Открытие окна full picture
+fullPicture.classList.remove('hidden');
+
+// Закрывание full picture с помощью ESC
+const body = document.querySelector('body');
+const buttonClose = document.querySelector('.big-picture__cancel');
+
+buttonClose.addEventListener('click', () => {
+  fullPicture.classList.add('hidden');
+  body.classList.remove('modal-open');
+});
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    fullPicture.classList.add('hidden');
+    body.classList.remove('modal-open');
+  }
 });
 
 // Создать комментарий и вствить в разметку
@@ -15,7 +27,7 @@ const userCommentsList = document.querySelector('.social__comments');
 const userCommentsTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
 const userCommentsFragment = document.createDocumentFragment();
 
-comments.forEach ((comment) => {
+const addComment = comments.forEach ((comment) => {
   const userCommentElement = userCommentsTemplate.cloneNode(true);
   userCommentsTemplate.querySelector('.social__picture').src = comment.avatar;
   userCommentsTemplate.querySelector('.social__picture').alt = comment.name;
@@ -27,10 +39,11 @@ userCommentsList.appendChild(userCommentsFragment);
 
 // Добавить элементы к full picture
 photoDescriptions.forEach ((picture) => {
-  fullPicture.querySelector('.big-picture__img').src = picture.url;
+  fullPicture.querySelector('.big-picture__img img').src = picture.url;
   fullPicture.querySelector('.likes-count').textContent = picture.likes;
   fullPicture.querySelector('.comments-count').textContent = picture.comments.length;
   fullPicture.querySelector('.social__caption').textContent = picture.description;
+  addComment();
 });
 
 // Скрыть счетчик комментариев и загрузку новых комментариев, остановить скролл
@@ -39,22 +52,3 @@ commentCount.classList.add('hidden');
 
 const newCommentAdding = fullPicture.querySelector('.comments-loader');
 newCommentAdding.classList.add('hidden');
-
-const body = document.querySelector('body');
-body.classList.add('modal-open');
-
-
-// Закрывание full picture с помощью ESC
-const buttonClose = document.querySelector('.big-picture__cancel');
-
-buttonClose.addEventListener('click', () => {
-  fullPicture.classList.add('hidden');
-  body.classList.remove('modal-open');
-});
-
-document.addEventListener('keydown', (evt) => {
-  if (evt.key ===  'Escape') {
-    fullPicture.classList.add('hidden');
-    body.classList.remove('modal-open');
-  }
-});
