7155bdf7587752388097b1ae59a51d19e414fb35
tkornilova <kornilova.tat07@gmail.com>
2021-11-12 23:48:17 -0700

    Загрузка данных с сервера

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-import './miniatures.js';
+import { renderUserMiniatures } from './miniatures.js';
 import './upload-photo.js';
 import './photo-editor.js';
+
+fetch('https://24.javascript.pages.academy/kekstagram/data')
+  .then((response) => response.json())
+  .then((userData) => {
+    renderUserMiniatures(userData);
+    console.log(userData);
+});
 
--- js/miniatures.js
+++ js/miniatures.js
@@ -1,23 +1,25 @@
-import { photoDescriptions } from './data.js';
+//import { photoDescriptions } from './data.js';
 import { renderFullPicture } from './full-picture.js';
 
 const userMiniatureElement = document.querySelector('.pictures');
 const userMiniatureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const userMiniaturesFragment = document.createDocumentFragment();
 
-const onCurrentMiniatureClick = (miniatureId) => {
-  const currentMiniature = photoDescriptions.find((miniature) => miniature.id === miniatureId);
-  renderFullPicture(currentMiniature);
-};
+export const renderUserMiniatures = (photoDescriptions) => {
+  const onCurrentMiniatureClick = (miniatureId) => {
+    const currentMiniature = photoDescriptions.find((miniature) => miniature.id === miniatureId);
+    renderFullPicture(currentMiniature);
+  };
 
-photoDescriptions.forEach ((miniature) => {
-  const miniatureElement = userMiniatureTemplate.cloneNode(true);
-  miniatureElement.querySelector('.picture__img').src = miniature.url;
-  miniatureElement.querySelector('.picture__comments').textContent = miniature.comments.length;
-  miniatureElement.querySelector('.picture__likes').textContent = miniature.likes;
-  miniatureElement.addEventListener('click', () => onCurrentMiniatureClick(miniature.id));
-  userMiniaturesFragment.appendChild(miniatureElement);
-});
+  photoDescriptions.forEach((miniature) => {
+    const miniatureElement = userMiniatureTemplate.cloneNode(true);
+    miniatureElement.querySelector('.picture__img').src = miniature.url;
+    miniatureElement.querySelector('.picture__comments').textContent = miniature.comments.length;
+    miniatureElement.querySelector('.picture__likes').textContent = miniature.likes;
+    miniatureElement.addEventListener('click', () => onCurrentMiniatureClick(miniature.id));
+    userMiniaturesFragment.appendChild(miniatureElement);
+  });
+}
 
 userMiniatureElement.appendChild(userMiniaturesFragment);
 
